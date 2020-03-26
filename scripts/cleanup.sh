bc5cc97d9cfb0ef9a7ce0c9651175af7a325847c
Jan de Muijnck-Hughes <jan.deMuijnck-Hughes@glasgow.ac.uk>
2020-03-26 09:58:35 +0000

    Updates to scripts.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -eux
 
-# Apt cleanup.
-apt autoremove
-apt update
+# delete all linux headers
+dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
 
-# Delete unneeded files.
-rm -f /home/ecoop2019/*.sh
+# this removes specific linux kernels, such as
+# linux-image-3.11.0-15-generic but
+# * keeps the current kernel
+# * does not touch the virtual packages, e.g.'linux-image-generic', etc.
+#
+dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
+
+# delete linux source
+dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+
+
+# Apt cleanup.
+apt-get -y autoremove
+apt-get -y clean
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
@@ -13,5 +26,3 @@ rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
 sync
-
-exit 0
--- scripts/setup-as-sudo.sh
+++ scripts/setup-as-sudo.sh
@@ -9,6 +9,14 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
+apt update
+
+# Upgrade all installed packages incl. kernel and kernel headers
+apt-get -y upgrade linux-server linux-headers-server
+
+# ensure the correct kernel headers are installed
+apt-get -y install linux-headers-$(uname -r)
+
 echo 'Begin Install'
 echo 'Installing /latest/ version of Haskell that Idris tests with.'
 
